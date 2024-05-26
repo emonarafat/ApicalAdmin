2df872629883eb6937619025559342fe31f3e0e4
Yaseer Arafat <yaseer.arafat@live.com>
2024-05-27 02:06:09 +0600

    Convert to net 8

---
+++ Apical/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Apical.Areas.Identity.Pages.Account.Manage;
+
+public class ResetAuthenticatorModel(
+	UserManager<IdentityUser> userManager,
+	SignInManager<IdentityUser> signInManager,
+	ILogger<ResetAuthenticatorModel> logger) : PageModel
+{
+
+	/// <summary>
+	///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+	///     directly from your code. This API may change or be removed in future releases.
+	/// </summary>
+	[TempData]
+    public string StatusMessage { get; set; }
+
+    public async Task<IActionResult> OnGet()
+    {
+        var user = await userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        var user = await userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+        }
+
+        await userManager.SetTwoFactorEnabledAsync(user, false);
+        await userManager.ResetAuthenticatorKeyAsync(user);
+        var userId = await userManager.GetUserIdAsync(user);
+        logger.LogInformation("User with ID '{UserId}' has reset their authentication app key.", user.Id);
+
+        await signInManager.RefreshSignInAsync(user);
+        StatusMessage = "Your authenticator app key has been reset, you will need to configure your authenticator app using the new key.";
+
+        return RedirectToPage("./EnableAuthenticator");
+    }
+}
